9ef27de9e3545e92f8c638d36bbe96acfd843965
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-08-26 11:08:00 +1200

    update

--- ecom_scripts/rename images.ps1
+++ ecom_scripts/rename images.ps1
@@ -2,16 +2,23 @@
 $txtfile = "C:\TEMP\missing_pic.txt"
 $imageSkus = Get-Content $txtfile
 #$missingitems = "one off item"
-$SourceDirectory = "\\xxxxxxxxxxxxxxxxxxxx"
-$DestinationDirectory = "xxxxxxxxxxxxxx"
+$mainDirectory = "\\xxxxxxxxxxxxxxxxxxxx"
+$main_modelDirectory = "xxxxxxxxxxxxxx"
+$altDirectory = "xxxxxxxxxxxxxx"
+$Alt_modelDirectory = "xxxxxxxxxxxxxx"
 
 
-#pull in directory
-$Folders = Get-ChildItem $SourceDirectory
+#FOR MAIN FOLDER
+$MainImages = Get-ChildItem $mainDirectory
 
-foreach ($Folder in $folders){
+foreach ($ItemSku in $ItemSkus){
 
+$Image = $MainImages | Where{$_.Name -Match "$ItemSku"}
+
+if ($image.count -eq "1") {
+    $image | Rename-Item -NewName $itensku_prod
     
+}
 
 #search through for images
 foreach($imageSku in $imageSkus){
