7934b14aa095c6ddf055ccb29b49029de758537a
Aaron Buchan <bgraaronb@briscoes-nz.co.nz>
2019-10-29 00:56:21 +1300

    Add logging

--- File Tools/Search Tools/Find and Move Images.ps1
+++ File Tools/Search Tools/Find and Move Images.ps1
@@ -1,5 +1,6 @@
 #set some variables
-$txtfile = "C:\TEMP\mp.txt"
+$logpath = "C:\Powershell\Logs"
+$txtfile = "C:\TEMP\missingitems.txt"
 $missingItems = Get-Content $txtfile
 #$missingitems = "one off item"
 $SourceDirectory = "\\xxxxxxxxxxxxxxxxxxxx"
@@ -9,22 +10,29 @@ $DestinationDirectory = "xxxxxxxxxxxxxx"
 $Folders = Get-ChildItem -Recurse $SourceDirectory
 
 #search through for images
-foreach($item in $missingitems){
-$item
-$pics = $folders | Where-Object{$_.Name -Match "$item" }
+foreach($MissingItem in $MissingItems){
+$MissingItem
+$items =""
+$items = $folders | Where-Object{$_.Name -Match "$MissingItem" }
 
+If($item.count -eq "0"){
+    $log = ""
+    $log = "There are no Items for " + $item
+    $log | Out-File -FilePath $logpath -Append
+}else {
 #if image is found move to a folder.
-foreach($pic in $pics){
-$pic | copy-item -destination $DestinationDirectory
-$name = $pic.name
+foreach($item in $items){
+$item | copy-item -destination $DestinationDirectory
+$name = $item.name
 $Testpath = "$DestinationDirectory\$name"
 
 #if image has been moved succesfully the write to log
 if (Test-Path $Testpath){
+$string = ""
 $string = $name + " Has been copied Successfully"
 $string
-$string | Out-File -FilePath "C:\temp\log\images.txt" -Append
+$string | Out-File -FilePath $logpath -Append
+            }
         } 
-        
     }
 }
