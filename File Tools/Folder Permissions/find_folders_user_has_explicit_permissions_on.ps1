763755e5a353c0587eabc0c3b2d91ca70d0265bc
BGR Aaron Buchan <bgraaronb@briscoes-nz.co.nz>
2019-10-29 14:01:09 +1300

    move to new directory

---
+++ File Tools/Folder Permissions/find_folders_user_has_explicit_permissions_on.ps1
@@ -0,0 +1,27 @@
+$directory = 'P:/'
+$txtfile = "C:\Temp"
+$users = get-content $txtfile
+$logpath = "C:\TEMP\permission log"
+#$directory = "C:\TEMP"
+
+$folders = get-childitem $directory -Recurse -erroraction ignore | where-object {$_.PSIsContainer} 
+
+
+foreach ($user in $users)
+{
+foreach($folder in $folders) {
+$ACL = $folder | get-acl
+$aclaccess = $ACL.access
+
+
+if ($aclaccess.IdentityReference -eq $user) {
+$folder.FullName
+$string = $user + " has permission on " + $folder.fullname
+$string | out-file -FilePath $logpath\$user.txt -append
+
+        }
+    }
+
+}
+
+
