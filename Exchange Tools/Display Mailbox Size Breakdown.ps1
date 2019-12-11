56ce20561c7488fd8f34f9c74be8775f860064c0
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2019-12-11 15:20:30 +1300

    Set URI as a variable

--- Exchange Tools/Display Mailbox Size Breakdown.ps1
+++ Exchange Tools/Display Mailbox Size Breakdown.ps1
@@ -1,6 +1,7 @@
 # Tool to Display a breakdown of a users mailbox and where the space is being used.
 
 #Sets some Hard Coded Variables
+$ExchangeURI = "http://exchange2k10/Powershell"
 $No = "n", "N", "No", "NO"
 $Yes = "y", ",Y", "Yes", "YES"
 
@@ -8,7 +9,7 @@ $Yes = "y", ",Y", "Yes", "YES"
 Write-Host " Use Your BGR Account for the pop up"
 $cred = get-credential
 #pulls in the commands from exchange server, and starts a remote session
-$Session = New-PSSession -configurationname Microsoft.Exchange -connectionURI http://exchange2k10/Powershell -credential $cred
+$Session = New-PSSession -configurationname Microsoft.Exchange -connectionURI $ExchangeURI -credential $cred
 
 #Imports the Exhange command set from the remote server
 Import-PSSession $Session -DisableNameChecking
@@ -17,7 +18,7 @@ Import-PSSession $Session -DisableNameChecking
 $User = Read-Host "Enter Username of mailbox"
 
 #runs through the users mailbox and exports a break down of where the space is being used.
-$mailbox = Get-MailboxFolderStatistics  -Identity briscoes\$user
+$mailbox = Get-MailboxFolderStatistics  -Identity technicalkiwi\$user
 $folders= ($mailbox | Select-Object Name,FolderPath,FolderSize,FolderAndSubfolderSize )
 $folders | Sort-Object FolderPath  |  Format-Table -AutoSize
 
