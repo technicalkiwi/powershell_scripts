4ed7d4b772b6d27c19bf954ccee2597e0f2da6eb
technicalkiwi <50688197+technicalkiwi@users.noreply.github.com>
2019-12-11 16:10:26 +1300

    update exchange URI

--- Exchange Tools/Remove user from all Distribution Groups.ps1
+++ Exchange Tools/Remove user from all Distribution Groups.ps1
@@ -2,24 +2,24 @@
 
 #Set some variables
 $LogPath = 'C:\TEMP\log\dist-groups.txt'
-$URI = 'http://exchange2k10/Powershell'
+$URI = 'http://TK-exchange/Powershell'
 
 #Create the log file
 Add-Content $LogPath "DISTRUBTION GROUP MEMBERSHIP"
 
 # Connects to the exchange server and imports a session
 #pop up box to ask for credential (use bgr account)
-Write-Host " Use Your BGR Account for the pop up"
+Write-Host " Use Your Admin Account for the pop up"
 $cred = get-credential
 #pulls in the commands from exchange server, and starts a remote session
-$Session = New-PSSession -configurationname Microsoft.Exchange -connectionURI $URI -credential $cred
+$Session = New-PSSession -configurationname Microsoft.Exchange -connectionURI $ExchangeURI -credential $cred
 
 #Imports the Exhange command set from the remote server
 Import-PSSession $Session -DisableNameChecking
 
 ####pulls in a list of all distibution groups
 $distgroups = Get-DistributionGroup
-#$distgroups = "Briscoe Group"
+
 
 foreach($distgroup in $distgroups.name){
 $DistGroupMembers = Get-DistributionGroupMember -Identity $distgroup 
