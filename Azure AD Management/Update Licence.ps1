6a7c2d88669a151be6ffb66fd658a05f9cfbe19f
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2020-06-04 19:35:31 +1200

    update script

--- Azure AD Management/Update Licence.ps1
+++ Azure AD Management/Update Licence.ps1
@@ -1,8 +1,46 @@
+$AzureAdCred = Get-Credential
+Connect-AzureAD -Credential $AzureAdCred
+
+$OldOfficePlan = "OFFICESUBSCRIPTION"
+$OldE3Plan = "ENTERPRISEPACK"
+$OFFICEPLAN = "O365_BUSINESS_PREMIUM"
+
+#$TEAMSPLAN = "TEAMS_COMMERCIAL_TRIAL"
+
+#$UserPrincipals = Get-AzureADUser -all $true | Select-Object userprincipalname
+$UserPrincipals = "adrian.limpin@briscoegroup.co.nz", "blair.cochrane@briscoegroup.onmicrosoft.com ", "Chris.Huang@briscoegroup.onmicrosoft.com ", "Krystal.moon@briscoegroup.onmicrosoft.com ", "russell.black@briscoegroup.co.nz", "stuartg@briscoegroup.co.nz" 
+
+foreach($UserPrincipal in $UserPrincipals){ #.userprincipalname){
+
+$UserPrincipal    
+# Remove Old Office Licence
+    # Get SkuID for Office License
+    $OldOfficeLicense = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
+    $OldOfficeLicense.SkuId = (Get-AzureADSubscribedSku | Where-Object -Property SkuPartNumber -Value $OLDOFFICEPLAN -EQ).SkuID
+    
+    # Create Object for Office Licence 
+    $OfficeLicensesToRemove = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+    $OfficeLicensesToRemove.AddLicenses = @()
+    $OfficeLicensesToRemove.RemoveLicenses = $OldOfficeLicense.SkuId
+    
+    # Assign Office Licence to User
+    Set-AzureADUserLicense -ObjectId $UserPrincipal -AssignedLicenses $OfficeLicensesToRemove
+
+#Remove Office E3 Licence
+    # Get SkuID for E3 Office License
+    $OldE3OfficeLicense = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
+    $OldE3OfficeLicense.SkuId = (Get-AzureADSubscribedSku | Where-Object -Property SkuPartNumber -Value $OLDE3PLAN -EQ).SkuID
+    
+    # Create Object for Office Licence 
+    $OfficeE3LicensesToRemove = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+    $OfficeE3LicensesToRemove.AddLicenses = @()
+    $OfficeE3LicensesToRemove.RemoveLicenses = $OldE3OfficeLicense.SkuId
+    
+    # Assign Office Licence to User
+    Set-AzureADUserLicense -ObjectId $UserPrincipal -AssignedLicenses $OfficeE3LicensesToRemove
 
 
-
-# Assign Office Licence if Required
-if($yes -contains $OfficeInstall){
+# Assign Office Licence
     # Get SkuID for Office License
     $OfficeLicense = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
     $OfficeLicense.SkuId = (Get-AzureADSubscribedSku | Where-Object -Property SkuPartNumber -Value $OFFICEPLAN -EQ).SkuID
@@ -13,19 +51,6 @@ if($yes -contains $OfficeInstall){
     
     # Assign Office Licence to User
     Set-AzureADUserLicense -ObjectId $UserPrincipal -AssignedLicenses $OfficeLicensesToAssign
-    }
-    
-    # Assign Teams Licence if Required
-    if($yes -contains $TeamsInstall){
-    
-    # Get SkuID for Teams Licence
-    $TeamsLicense = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
-    $TeamsLicense.SkuId = (Get-AzureADSubscribedSku | Where-Object -Property SkuPartNumber -Value $TEAMSPLAN -EQ).SkuID
-    
-    # Create Object for Teams Licence
-    $TeamsLicensesToAssign = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
-    $TeamsLicensesToAssign.AddLicenses = $TeamsLicense
-    
-    # Assign Teams Licence to User
-    Set-AzureADUserLicense -ObjectId $UserPrincipal -AssignedLicenses $TeamsLicensesToAssign
-    }
+   
+}
+   
